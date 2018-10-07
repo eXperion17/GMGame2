7c1fefbf7f5b31503a68d96558b53c9a7ba26e22
eXperion17 <marc_gomersbach@hotmail.com>
2018-10-07 12:57:56 +0200

    Initial Project Setup & Pressure Plate object copleted

---
+++ Assets/Scripts/Objects/PressureTrigger.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class PressureTrigger : MonoBehaviour
+{
+    //Audio
+    [SerializeField]
+    private AudioSource onPress;
+    [SerializeField]
+    private AudioSource onRelease;
+
+    [SerializeField]
+    private Vector3 movementOnPress;
+    [SerializeField]
+    private bool beingPressed;
+    [SerializeField]
+    private float movementPerTick;
+
+    private Vector3 startPosition;
+        
+    //Events
+    public UnityEvent onPressEvent;
+    public UnityEvent onReleaseEvent;
+
+    private IEnumerator movementRoutine;
+
+    private void Start()
+    {
+        startPosition = transform.position;
+        movementRoutine = Movement();
+        StartCoroutine(movementRoutine);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            beingPressed = true;
+            onPressEvent.Invoke();
+            if (onPress)
+                onPress.Play();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            beingPressed = false;
+            onReleaseEvent.Invoke();
+            if (onRelease)
+                onRelease.Play();
+        }
+    }
+
+    private IEnumerator Movement()
+    {
+        while (true)
+        {
+            if (beingPressed)
+            {
+                if (transform.position.y > (startPosition.y + movementOnPress.y))
+                {
+                    transform.Translate(movementOnPress * movementPerTick);
+                }
+            }
+            else
+            {
+                if (transform.position.y <= startPosition.y)
+                {
+                    transform.Translate(movementOnPress * -movementPerTick);
+                }
+            }
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+}
